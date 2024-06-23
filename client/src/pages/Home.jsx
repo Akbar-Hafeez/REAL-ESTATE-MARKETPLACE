db9df76315e47ee8176a062e189dcf4f54e369ff
Akbar Hafeez <akbarhafeez25@gmail.com>
2024-06-24 03:58:46 +0500

    swiper updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
+import { Navigation,Autoplay } from 'swiper/modules';
 import SwiperCore from 'swiper';
 import 'swiper/css/bundle';
 import ListingItem from '../components/ListingItem';
@@ -18,8 +18,8 @@ export default function Home() {
   const [offerListings, setOfferListings] = useState([]);
   const [saleListings, setSaleListings] = useState([]);
   const [rentListings, setRentListings] = useState([]);
-  SwiperCore.use([Navigation]);
-  console.log(offerListings);
+  SwiperCore.use([Autoplay,Navigation]);
+ 
   useEffect(() => {
     const fetchOfferListings = async () => {
       try {
